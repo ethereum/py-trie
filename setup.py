7dc9a65f16b01d12c9ce9c7c2803c501a6680f98
Piper Merriam <pipermerriam@gmail.com>
2017-11-28 09:52:59 -0700

    Github migration, python2, bumpversion

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
 from setuptools import setup, find_packages
-
-
-DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-readme = open(os.path.join(DIR, 'README.md')).read()
 
 
 setup(
     name='trie',
-    version="0.3.0",
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.3.0',
     description="""Python implementation of the Ethereum Trie structure""",
-    long_description=readme,
+    long_description_markdown_filename='README.md',
     author='Piper Merriam',
     author_email='pipermerriam@gmail.com',
-    url='https://github.com/pipermerriam/py-trie',
+    url='https://github.com/ethereum/py-trie',
     include_package_data=True,
     py_modules=['trie'],
+    setup_requires=['setuptools-markdown'],
     install_requires=[
-        "ethereum-utils>=0.2.0",
+        "eth-utils>=0.5.0",
         "rlp==0.4.7",
     ],
     license="MIT",
