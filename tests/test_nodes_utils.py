c5e8000ada500cf3d1ee1ebca6cd3c2e61814bd8
NIC619 <twedusuck@gmail.com>
2017-12-12 15:52:01 +0800

    add expected outs to valid test suite and combine valid/invalid test suites

--- tests/test_nodes_utils.py
+++ tests/test_nodes_utils.py
@@ -51,102 +51,82 @@ def test_consume_common_prefix(left, right, expected):
 
 
 @pytest.mark.parametrize(
-    'valid_node',
+    'node,expected_output',
     (
-        (b"\x00\x03\x04\x05\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b"\x01\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b"\x02value"),  # noqa: E501
+        (b"\x00\x03\x04\x05\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p",    # noqa: E501
+            (0, b'\x00\x00\x01\x01\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p")),  # noqa: E501
+        (b"\x01\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p",  # noqa: E501
+            (1, b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p")),  # noqa: E501
+        (b"\x02value",
+            (2, None, b'value')),
+        (b'', None),
+        (None, None),
+        (b"\x00\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b"\x01\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b"\x01\x02\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b'\x02', None),
     ),
 )
-def test_binary_trie_node_parsing_with_valid_input(valid_node):
-    parse_node(valid_node)
+def test_binary_trie_node_parsing(node, expected_output):
+    if expected_output:
+        assert expected_output == parse_node(node)
+    else:
+        with pytest.raises(InvalidNode):
+            parse_node(node)
 
 
 @pytest.mark.parametrize(
-    'invalid_node',
+    'keypath,node,expected_output',
     (
-        (b''),
-        (None),
-        (b"\x00\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b"\x01\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b"\x01\x02\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b'\x02'),
+        (b'\x00', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", b"\x00\x10\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
+        (b'', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b'\x00', b"\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b'\x00', 12345, None),
+        (b'\x00', range(32), None),
+        (b'\x01', b"\x00\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b'\x02', b'', None),
     ),
 )
-def test_binary_trie_node_parsing_with_invalid_input(invalid_node):
-    with pytest.raises(InvalidNode):
-        parse_node(invalid_node)
+def test_encode_binary_trie_kv_node(keypath, node, expected_output):
+    if expected_output:
+        assert expected_output == encode_kv_node(keypath, node)
+    else:
+        with pytest.raises(ValidationError):
+            encode_kv_node(keypath, node)
 
 
 @pytest.mark.parametrize(
-    'keypath,valid_value',
+    'left_child_node_hash,right_child_node_hash,expected_output',
     (
-        (b'\x00', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
+        (b'\xc8\x9e\xfd\xaaT\xc0\xf2\x0cz\xdfa(\x82\xdf\tP\xf5\xa9Qc~\x03\x07\xcd\xcbLg/)\x8b\x8b\xc6', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p",  # noqa: E501
+            (b"\x01\xc8\x9e\xfd\xaaT\xc0\xf2\x0cz\xdfa(\x82\xdf\tP\xf5\xa9Qc~\x03\x07\xcd\xcbLg/)\x8b\x8b\xc6\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p")),  # noqa: E501
+        (b'', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", None),  # noqa: E501
+        (b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", b'\x01', None),  # noqa: E501
+        (b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", 12345, None),  # noqa: E501
+        ([3] * 32, [4] * 32, None),
+        (b'\x01' * 33, b'\x01' * 32, None),
     ),
 )
-def test_encode_binary_trie_kv_node_with_valid_input(keypath, valid_value):
-    encode_kv_node(keypath, valid_value)
-        
-        
-@pytest.mark.parametrize(
-    'keypath,invalid_node',
-    (
-        (b'', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b'\x00', b"\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b'\x00', 12345),
-        (b'\x00', range(32)),
-        (b'\x01', b"\x00\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b'\x02', b''),
-    ),
-)
-def test_encode_binary_trie_kv_node_with_invalid_input(keypath, invalid_node):
-    with pytest.raises(ValidationError):
-        encode_kv_node(keypath, invalid_node)
+def test_encode_binary_trie_branch_node(left_child_node_hash, right_child_node_hash, expected_output):
+    if expected_output:
+        assert expected_output == encode_branch_node(left_child_node_hash, right_child_node_hash)
+    else:
+        with pytest.raises(ValidationError):
+            encode_branch_node(left_child_node_hash, right_child_node_hash)
 
 
 @pytest.mark.parametrize(
-    'left_child_node_hash,right_child_node_hash',
+    'value,expected_output',
     (
-        (b'\xc8\x9e\xfd\xaaT\xc0\xf2\x0cz\xdfa(\x82\xdf\tP\xf5\xa9Qc~\x03\x07\xcd\xcbLg/)\x8b\x8b\xc6', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
+        (b'\x03\x04\x05', b'\x02\x03\x04\x05'),
+        (b'', None),
+        (12345, None),
+        (range(5), None),
     ),
 )
-def test_encode_binary_trie_branch_node_with_valid_input(left_child_node_hash, right_child_node_hash):
-    encode_branch_node(left_child_node_hash, right_child_node_hash)
-        
-        
-@pytest.mark.parametrize(
-    'invalid_left_child_node_hash,invalid_right_child_node_hash',
-    (
-        (b'', b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p"),  # noqa: E501
-        (b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", b'\x01'),  # noqa: E501
-        (b"\xc5\xd2F\x01\x86\xf7#<\x92~}\xb2\xdc\xc7\x03\xc0\xe5\x00\xb6S\xca\x82';{\xfa\xd8\x04]\x85\xa4p", 12345),  # noqa: E501
-        ([3] * 32, [4] * 32),
-        (b'\x01' * 33, b'\x01' * 32),
-    ),
-)
-def test_encode_binary_trie_branch_node_with_invalid_input(invalid_left_child_node_hash, invalid_right_child_node_hash):
-    with pytest.raises(ValidationError):
-        encode_branch_node(invalid_left_child_node_hash, invalid_right_child_node_hash)
-
-
-@pytest.mark.parametrize(
-    'valid_value',
-    (
-        (b'\x01\x02x\03'),
-    ),
-)
-def test_encode_binary_trie_leaf_node_with_invalid_input(valid_value):
-    encode_leaf_node(valid_value)
-        
-        
-@pytest.mark.parametrize(
-    'invalid_value',
-    (
-        (b''),
-        (12345),
-        (range(5)),
-    ),
-)
-def test_encode_binary_trie_leaf_node_with_invalid_input(invalid_value):
-    with pytest.raises(ValidationError):
-        encode_leaf_node(invalid_value)
+def test_encode_binary_trie_leaf_node(value, expected_output):
+    if expected_output:
+        assert expected_output == encode_leaf_node(value)
+    else:
+        with pytest.raises(ValidationError):
+            encode_leaf_node(value)
