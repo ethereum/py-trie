c2154a50fb007dee90b026381380e7963ac4d777
NIC619 <twedusuck@gmail.com>
2017-12-20 12:28:06 +0900

    update random number generator and a direct list evaluation of zipped kv pair

--- tests/test_bin_trie.py
+++ tests/test_bin_trie.py
@@ -1,13 +1,7 @@
-import random
-
 from hypothesis import (
     given,
     strategies as st,
     settings,
-)
-
-from eth_utils import (
-    to_list,
 )
 
 from trie.trie import (
@@ -17,18 +11,13 @@ from trie.constants import (
     BLANK_HASH,
 )
 
-@to_list
-def to_kv_pairs(kv_zip):
-    for k, v in kv_zip:
-        yield (k, v)
-
 
 @given(k=st.lists(st.binary(min_size=32, max_size=32), min_size=100, max_size=100, unique=True),
     v=st.lists(st.binary(min_size=1), min_size=100, max_size=100),
-    set_random=st.random_module())
+    random=st.randoms())
 @settings(max_examples=10)
-def test_bin_trie_different_order_insert(k, v, set_random):
-    kv_pairs = to_kv_pairs(zip(k, v))
+def test_bin_trie_different_order_insert(k, v, random):
+    kv_pairs = list(zip(k, v))
     result = BLANK_HASH
     # Repeat 3 times
     for _ in range(3):
@@ -47,5 +36,3 @@ def test_bin_trie_different_order_insert(k, v, set_random):
         for k, v in kv_pairs:
             trie.delete(k)
         assert trie.root_hash == BLANK_HASH
-
-# TODO: add more binary trie tests
