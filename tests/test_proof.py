e72ebe70ee88f2b5ff88d2f5ddcc2d9e7e81eabf
Piper Merriam <pipermerriam@gmail.com>
2017-12-20 12:01:55 -0700

    relocate proof to classmethod on Trie

--- tests/test_proof.py
+++ tests/test_proof.py
@@ -1,32 +1,30 @@
 import pytest
 
+from trie import Trie
 from trie.exceptions import BadTrieProof
-from trie.proof import verify_proof
 from trie.utils.sha3 import keccak
 
 
-def test_verify_proof_key_exists():
+def test_get_from_proof_key_exists():
     from .sample_proof_key_exists import key, state_root, proof
-    assert verify_proof(state_root, key, proof) != b''
+    assert Trie.get_from_proof(state_root, key, proof) != b''
 
 
-def test_verify_proof_key_does_not_exist():
+def test_get_from_proof_key_does_not_exist():
     from .sample_proof_key_does_not_exist import key, state_root, proof
-    assert verify_proof(state_root, key, proof) == b''
+    assert Trie.get_from_proof(state_root, key, proof) == b''
 
 
-def test_verify_proof_invalid():
+def test_get_from_proof_invalid():
     from .sample_proof_key_exists import key, state_root, proof
     proof[5][3] = b''
     with pytest.raises(BadTrieProof):
-        verify_proof(state_root, key, proof)
+        Trie.get_from_proof(state_root, key, proof)
 
 
-def test_verify_proof_empty():
+def test_get_from_proof_empty():
     state_root = keccak(b'state root')
     key = keccak(b'some key')
     proof = []
     with pytest.raises(BadTrieProof):
-        verify_proof(state_root, key, proof)
-
-
+        Trie.get_from_proof(state_root, key, proof)
