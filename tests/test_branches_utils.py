1787bbfc5f4db4fcde14397df83f6138586609b2
NIC619 <twedusuck@gmail.com>
2017-12-22 16:50:48 +0900

    add if_branch_exist and tests

---
+++ tests/test_branches_utils.py
@@ -0,0 +1,33 @@
+import pytest
+
+from trie.trie import (
+    BinaryTrie,
+)
+from trie.utils.branches import (
+    if_branch_exist,
+)
+
+
+def set_up_test_trie():
+    trie = BinaryTrie(db={})
+    trie.set(b'\x12\x34\x56\x78\x9a', b'9a')
+    trie.set(b'\x12\x34\x56\x78\x9b', b'9b')
+    trie.set(b'\x12\x34\x56\xff', b'ff')
+
+    return trie
+
+
+@pytest.mark.parametrize(
+    'key_prefix,if_exist',
+    (
+        (b'\x12\x34', True),
+        (b'\x12\x34\x56\x78\x9b', True),
+        (b'\x12\x56', False),
+        (b'\x12\x34\x56\xff\xff', False),
+        (b'\x12\x34\x56', True),
+        (b'\x12\x34\x56\x78', True),
+    ),
+)
+def test_branch_exist(key_prefix, if_exist):
+    test_trie = set_up_test_trie()
+    assert if_branch_exist(test_trie.db, test_trie.root_hash, key_prefix) == if_exist
