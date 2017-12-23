4d7fb24a3517f8cc6576a74a43f0ac8edda95cdc
NIC619 <twedusuck@gmail.com>
2017-12-24 00:29:21 +0900

    refactor test_bin_trie_delete_subtrie test suite

--- tests/test_bin_trie.py
+++ tests/test_bin_trie.py
@@ -43,42 +43,35 @@ def test_bin_trie_different_order_insert(k, v, random):
         assert trie.root_hash == BLANK_HASH
 
 
-def test_bin_trie_delete_subtrie():
+@pytest.mark.parametrize(
+    'kv1,kv2,key_to_be_deleted,will_delete,will_rasie_error',
+    (
+        ((b'\x12\x34\x56\x78', b'78'), (b'\x12\x34\x56\x79', b'79'), b'\x12\x34\x56', True, False),
+        ((b'\x12\x34\x56\x78', b'78'), (b'\x12\x34\x56\xff', b'ff'), b'\x12\x34\x56', True, False),
+        ((b'\x12\x34\x56\x78', b'78'), (b'\x12\x34\x56\x79', b'79'), b'\x12\x34\x57', False, False),
+        ((b'\x12\x34\x56\x78', b'78'), (b'\x12\x34\x56\x79', b'79'), b'\x12\x34\x56\x78\x9a', False, True),
+    ),
+)
+def test_bin_trie_delete_subtrie(kv1, kv2, key_to_be_deleted, will_delete, will_rasie_error):
     trie = BinaryTrie(db={})
     # First test case, delete subtrie of a kv node
-    trie.set(b'\x12\x34\x56\x78', b'78')
-    trie.set(b'\x12\x34\x56\x79', b'79')
-    assert trie.get(b'\x12\x34\x56\x78') == b'78'
-    assert trie.get(b'\x12\x34\x56\x79') == b'79'
+    trie.set(kv1[0], kv1[1])
+    trie.set(kv2[0], kv2[1])
+    assert trie.get(kv1[0]) == kv1[1]
+    assert trie.get(kv2[0]) == kv2[1]
 
-    trie.delete_subtrie(b'\x12\x34\x56')
-    assert trie.get(b'\x12\x34\x56\x78') == None
-    assert trie.get(b'\x12\x34\x56\x79') == None
-    assert trie.root_hash == BLANK_HASH
-
-    # Second test case, delete subtrie of a branch node
-    trie.set(b'\x12\x34\x56\x78', b'78')
-    trie.set(b'\x12\x34\x56\xff', b'ff')
-    assert trie.get(b'\x12\x34\x56\x78') == b'78'
-    assert trie.get(b'\x12\x34\x56\xff') == b'ff'
-
-    trie.delete_subtrie(b'\x12\x34\x56')
-    assert trie.get(b'\x12\x34\x56\x78') == None
-    assert trie.get(b'\x12\x34\x56\xff') == None
-    assert trie.root_hash == BLANK_HASH
-
-    # Third test case, delete subtrie with non-exist key
-    trie.set(b'\x12\x34\x56\x78', b'78')
-    trie.set(b'\x12\x34\x56\x79', b'79')
-    assert trie.get(b'\x12\x34\x56\x78') == b'78'
-    assert trie.get(b'\x12\x34\x56\x79') == b'79'
-    root_hash_before_delete = trie.root_hash
-
-    trie.delete_subtrie(b'\x12\x34\x57')
-    assert trie.get(b'\x12\x34\x56\x78') == b'78'
-    assert trie.get(b'\x12\x34\x56\x79') == b'79'
-    assert trie.root_hash == root_hash_before_delete
-
-    # Fourth test case, delete subtrie with key too long
-    with pytest.raises(LeafNodeOverrideError):
-        trie.delete_subtrie(b'\x12\x34\x56\x78\x9a')
+    if will_delete:
+        trie.delete_subtrie(key_to_be_deleted)
+        assert trie.get(kv1[0]) == None
+        assert trie.get(kv2[0]) == None
+        assert trie.root_hash == BLANK_HASH
+    else:
+        if will_rasie_error:
+            with pytest.raises(LeafNodeOverrideError):
+                trie.delete_subtrie(key_to_be_deleted)
+        else:
+            root_hash_before_delete = trie.root_hash
+            trie.delete_subtrie(key_to_be_deleted)
+            assert trie.get(kv1[0]) == kv1[1]
+            assert trie.get(kv2[0]) == kv2[1]
+            assert trie.root_hash == root_hash_before_delete
