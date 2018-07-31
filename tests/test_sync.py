a95be4f418da443c41293906259294fe36d3137b
Guilherme Salgado <gsalgado@gmail.com>
2018-07-31 12:09:25 +0200

    wip: convert HexaryTrieSync methods to async

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 from hypothesis import (
@@ -25,22 +26,36 @@ EXAMPLE_37968 = 37968
 EXAMPLE_809368 = 809368
 
 
+class AsyncDB(dict):
+
+    async def coro_exists(self, key):
+        return key in self
+
+    async def coro_set(self, key, val):
+        self[key] = val
+
+
 @given(random=strategies.randoms())
 @settings(max_examples=50)
 @example(random=RandomWithSeed(EXAMPLE_37968))
 @example(random=RandomWithSeed(EXAMPLE_809368))
 def test_trie_sync(random):
-    src_trie, contents = make_random_trie(random)
 
-    dest_db = {}
-    scheduler = HexaryTrieSync(src_trie.root_hash, dest_db, logger)
-    requests = scheduler.next_batch()
-    while len(requests) > 0:
-        results = []
-        for request in requests:
-            results.append([request.node_key, src_trie.db[request.node_key]])
-        scheduler.process(results)
-        requests = scheduler.next_batch(10)
-    dest_trie = HexaryTrie(dest_db, src_trie.root_hash)
-    for key, value in contents.items():
-        assert dest_trie[key] == value
+    # Apparently hypothesis tests cannot be used in conjunction with pytest-asyncio yet, so do it
+    # like this for now. https://github.com/HypothesisWorks/hypothesis/pull/1343
+    async def _test_trie_sync():
+        src_trie, contents = make_random_trie(random)
+        dest_db = AsyncDB()
+        scheduler = HexaryTrieSync(src_trie.root_hash, dest_db, logger)
+        requests = scheduler.next_batch()
+        while len(requests) > 0:
+            results = []
+            for request in requests:
+                results.append([request.node_key, src_trie.db[request.node_key]])
+            await scheduler.process(results)
+            requests = scheduler.next_batch(10)
+        dest_trie = HexaryTrie(dest_db, src_trie.root_hash)
+        for key, value in contents.items():
+            assert dest_trie[key] == value
+
+    asyncio.get_event_loop().run_until_complete(_test_trie_sync())
