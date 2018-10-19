5cd74f8f1435d5408e8f04285701de243eb6f257
6ug <bugcrowd.yvt@gmail.com>
2018-10-19 14:32:39 +0530

    Removed cytoolz hard dependency

--- setup.py
+++ setup.py
@@ -17,9 +17,8 @@ setup(
     setup_requires=['setuptools-markdown'],
     install_requires=[
         "eth-hash>=0.1.0,<1.0.0",
-        "eth-utils>=1.0.1,<2.0.0",
+        "eth-utils>=1.3.0,<2.0.0",
         "rlp>=0.4.7,<2.0.0",
-        "cytoolz>=0.8.0,<1",
     ],
     license="MIT",
     zip_safe=False,
