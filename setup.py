aea8c42e1bd648048aa14da61edf60a4f47ec2ef
Piper Merriam <piper@pipermerriam.com>
2018-11-12 13:11:40 -0700

    fix eth-utils version constraints

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
 
     install_requires=[
         "eth-hash>=0.1.0,<1.0.0",
-        "eth-utils>=1.3.0b0,<2.0.0a0",
+        "eth-utils>=1.3.0,<2.0.0",
         "rlp>=1,<2",
     ],
     extras_require=extras_require,
