5b13b4f5cc75d8f2b461c99c7612394477ff6ec9
Daehee Kim <daehee.kim@theloop.co.kr>
2019-01-29 10:31:12 +0900

    Add a test for proof of nodes less than 32 bytes.

--- tests/test_proof.py
+++ tests/test_proof.py
@@ -31,3 +31,18 @@ def test_get_from_proof_empty():
     proof = []
     with pytest.raises(BadTrieProof):
         HexaryTrie.get_from_proof(state_root, key, proof)
+
+
+def test_get_from_proof_node_less_than_32bytes():
+    t = HexaryTrie(db={})
+    t[b'some key'] = b'some value'
+    assert HexaryTrie.get_from_proof(t.root_hash, b'some key', [t.root_node]) == b'some value'
+
+    t[b'some key2'] = b'some value2'
+
+    proof = [
+        t.root_node,
+        [b'', b'', b'', [b'2', b'some value2'], b'', b'', b'',
+         b'', b'', b'', b'', b'', b'', b'', b'', b'', b'some value']
+    ]
+    assert HexaryTrie.get_from_proof(t.root_hash, b'some key2', proof) == b'some value2'
