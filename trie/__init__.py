a60b249b2e0ae3483a2acc805b76a4822ea5ab63
Jason Carver <ut96caarrs@snkmail.com>
2020-05-06 14:59:20 -0700

    Remove trie.Trie in favor of trie.HexaryTrie

--- trie/__init__.py
+++ trie/__init__.py
@@ -1,5 +1,4 @@
 import pkg_resources
-import warnings
 
 from .binary import (  # noqa: F401
     BinaryTrie,
@@ -7,16 +6,6 @@ from .binary import (  # noqa: F401
 from .hexary import (  # noqa: F401
     HexaryTrie,
 )
-
-
-class Trie(HexaryTrie):
-    def __init__(self, *args, **kwargs):
-        warnings.warn(DeprecationWarning(
-            "The `trie.Trie` class has been renamed to `trie.HexaryTrie`. "
-            "Please update your code as the `trie.Trie` class will be removed in "
-            "a subsequent release"
-        ))
-        super().__init__(*args, **kwargs)
 
 
 __version__ = pkg_resources.get_distribution("trie").version
