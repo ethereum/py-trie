aaa8549cae7192c9bfabd3d0a580dc6b9ca61da6
Jason Carver <ut96caarrs@snkmail.com>
2020-08-31 11:24:32 -0700

    Drop pypy support

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
