beac2c4918e7dfc9a1e9661bffd74e22c720bf0c
kclowes <kclowes@users.noreply.github.com>
2021-11-22 16:02:49 -0700

    Update eth-utils

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
 
     install_requires=[
         "eth-hash>=0.1.0,<1.0.0",
-        "eth-utils>=1.6.1,<2.0.0",
+        "eth-utils>=2.0.0,<3.0.0",
         "hexbytes>=0.2.0,<0.3.0",
         "rlp>=1,<3",
         "sortedcontainers>=2.1.0,<3",
