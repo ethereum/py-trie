26ae2ab34073a963163bdb867be91461d0ed01eb
fselmo <fselmo2@gmail.com>
2023-10-31 10:16:15 -0600

    Put back more breaking changes related to dropping py37

--- trie/__init__.py
+++ trie/__init__.py
@@ -1,12 +1,6 @@
-from importlib.metadata import (
-    version as __version,
-)
+import pkg_resources
 
-from .binary import (
-    BinaryTrie,
-)
-from .hexary import (
-    HexaryTrie,
-)
+from .binary import BinaryTrie  # noqa: F401
+from .hexary import HexaryTrie  # noqa: F401
 
-__version__ = __version("trie")
+__version__ = pkg_resources.get_distribution("trie").version
