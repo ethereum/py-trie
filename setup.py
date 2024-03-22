7b5495d0a39fcf5cec4519281d724dcef55157b0
pacrob <5199899+pacrob@users.noreply.github.com>
2024-03-22 10:20:24 -0600

    open dep version hexbytes>=0.2.3

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         "eth-hash>=0.1.0",
         "eth-utils>=2.0.0",
-        "hexbytes>=0.2.0,<0.4.0",
+        "hexbytes>=0.2.3",
         "rlp>=3",
         "sortedcontainers>=2.1.0",
     ],
