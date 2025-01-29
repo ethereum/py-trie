d92445c201c1552906c4a1551c1ca703883aef2e
pacrob <5199899+pacrob@users.noreply.github.com>
2025-01-29 10:51:15 -0700

    Bump version: 3.0.1 → 3.1.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.md") as readme_file:
 setup(
     name="trie",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bump-my-version' utility.
-    version="3.0.1",
+    version="3.1.0",
     description="""Python implementation of the Ethereum Trie structure""",
     long_description=long_description,
     long_description_content_type="text/markdown",
